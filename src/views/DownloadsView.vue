24254208d16dd412e3b9c352b2dafbbc16a1c413
Nick Cao <nick.cao@avcrm.net>
2025-12-21 19:30:16 +1030

    Fix release date to December 21, 2025

--- src/views/DownloadsView.vue
+++ src/views/DownloadsView.vue
@@ -15,7 +15,7 @@ const platforms = ref<Platform[]>([
     name: 'macOS',
     icon: 'M18.71 19.5c-.83 1.24-1.71 2.45-3.05 2.47-1.34.03-1.77-.79-3.29-.79-1.53 0-2 .77-3.27.82-1.31.05-2.3-1.32-3.14-2.53C4.25 17 2.94 12.45 4.7 9.39c.87-1.52 2.43-2.48 4.12-2.51 1.28-.02 2.5.87 3.29.87.78 0 2.26-1.07 3.81-.91.65.03 2.47.26 3.64 1.98-.09.06-2.17 1.28-2.15 3.81.03 3.02 2.65 4.03 2.68 4.04-.03.07-.42 1.44-1.38 2.83M13 3.5c.73-.83 1.94-1.46 2.94-1.5.13 1.17-.34 2.35-1.04 3.19-.69.85-1.83 1.51-2.95 1.42-.15-1.15.41-2.35 1.05-3.11z',
     version: '1.0.3',
-    releaseDate: '2024-12-21',
+    releaseDate: '2025-12-21',
     downloadUrl: 'https://github.com/xicv/recogniz.ing/releases/download/v1.0.3/recognizing-1.0.3-macos.zip',
     changelog: [
       'Fixed macOS Gatekeeper verification issues',
@@ -28,7 +28,7 @@ const platforms = ref<Platform[]>([
     name: 'Windows',
     icon: 'M3,12V6.75L9,5.43v6.48L3,12M20,3v8.75L10,11.9V5.21L20,3M3,13l6,.09V19.9L3,18.75V13m17,.25V22L10,20.09v-7Z',
     version: '1.0.3',
-    releaseDate: '2024-12-21',
+    releaseDate: '2025-12-21',
     downloadUrl: 'https://github.com/xicv/recogniz.ing/releases/download/v1.0.3/recognizing-1.0.3-windows.exe',
     changelog: [
       'Initial Windows release',
@@ -215,7 +215,7 @@ const downloadPlatform = (platform: Platform) => {
             <div class="bg-white rounded-lg border border-slate-200 p-8">
               <div class="flex items-center justify-between mb-4">
                 <h3 class="text-xl font-medium">Version 1.0.3</h3>
-                <span class="text-sm text-slate-500">December 21, 2024</span>
+                <span class="text-sm text-slate-500">December 21, 2025</span>
               </div>
               <ul class="space-y-2 text-slate-600">
                 <li class="flex items-start">
